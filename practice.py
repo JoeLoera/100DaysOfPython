e569fb963f8cbe1f82cbf43fa94a588d7f3c34ff
Joe Loera <joeloera7@gmail.com>
2024-07-31 22:09:22 -0700

    Starting code for password generator project

--- practice.py
+++ practice.py
@@ -66,59 +66,77 @@
 # else:
 #     print("You fell into a hole. Game Over.")
 
+# import random
+
+# rock = '''
+#     _______
+# ---'   ____)
+#       (_____)
+#       (_____)
+#       (____)
+# ---.__(___)
+# '''
+
+# paper = '''
+#     _______
+# ---'   ____)____
+#           ______)
+#           _______)
+#          _______)
+# ---.__________)
+# '''
+
+# scissors = '''
+#     _______
+# ---'   ____)____
+#           ______)
+#        __________)
+#       (____)
+# ---.__(___)
+# '''
+
+# #Write your code below this line 👇
+
+# game_images = [rock, paper, scissors]
+
+# user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+# if user_choice > 3 or user_choice < 0:
+#     print("You typed an invalid number, you lose!")
+# else:
+#     print(game_images[user_choice])
+    
+#     computer_choice = random.randint(0, 2)
+#     print("Computer chose:")
+#     print(game_images[computer_choice])
+    
+    
+    
+#     if user_choice == 0 and computer_choice == 2:
+#         print("You win!")
+#     elif computer_choice == 0 and user_choice == 2:
+#         print("You lose!")
+#     elif computer_choice > user_choice:
+#         print("You lose!")
+#     elif user_choice > computer_choice:
+#         print("You win!")
+#     elif computer_choice == user_choice:
+#         print("It's a draw!")
+    
+
+#Password Generator Project
 import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-rock = '''
-    _______
----'   ____)
-      (_____)
-      (_____)
-      (____)
----.__(___)
-'''
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-paper = '''
-    _______
----'   ____)____
-          ______)
-          _______)
-         _______)
----.__________)
-'''
+#Eazy Level - Order not randomised:
+#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
 
-scissors = '''
-    _______
----'   ____)____
-          ______)
-       __________)
-      (____)
----.__(___)
-'''
 
-#Write your code below this line 👇
-
-game_images = [rock, paper, scissors]
-
-user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
-if user_choice > 3 or user_choice < 0:
-    print("You typed an invalid number, you lose!")
-else:
-    print(game_images[user_choice])
-    
-    computer_choice = random.randint(0, 2)
-    print("Computer chose:")
-    print(game_images[computer_choice])
-    
-    
-    
-    if user_choice == 0 and computer_choice == 2:
-        print("You win!")
-    elif computer_choice == 0 and user_choice == 2:
-        print("You lose!")
-    elif computer_choice > user_choice:
-        print("You lose!")
-    elif user_choice > computer_choice:
-        print("You win!")
-    elif computer_choice == user_choice:
-        print("It's a draw!")
-    
+#Hard Level - Order of characters randomised:
+#e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
