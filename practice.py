d90eb7c1c079537b7b470d6a43a0b7ef17a5b376
Joe Loera <joeloera7@gmail.com>
2024-08-02 06:54:30 -0700

    Easy Password solution

--- practice.py
+++ practice.py
@@ -137,8 +137,18 @@ nr_numbers = int(input(f"How many numbers would you like?\n"))
 #Eazy Level - Order not randomised:
 #e.g. 4 letter, 2 symbol, 2 number = JduE&!91
 
+password = ""
+#nr_letters = 4
+for char in range (1, nr_letters + 1):
+    password += random.choice(letters)
 
+for char in range (1, nr_symbols + 1):
+    password += random.choice(symbols)
+
+for char in range (1, nr_numbers + 1):
+    password += random.choice(numbers)
+
+print(password)
 #Hard Level - Order of characters randomised:
 #e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
 
-# Still need to work this out
